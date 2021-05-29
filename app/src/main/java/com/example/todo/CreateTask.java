ef2b94c5147e6358d03979b467a8cdb467fbc1e8
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-05-29 15:49:38 +0100

    added checks for create task

--- app/src/main/java/com/example/todo/CreateTask.java
+++ app/src/main/java/com/example/todo/CreateTask.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateTask extends Fragment {
 
@@ -28,23 +29,23 @@ public class CreateTask extends Fragment {
         View view = inflater.inflate(R.layout.fragment_create_task, container, false);
         Button button = view.findViewById(R.id.fragment_create_task_button);
 
+        // Add back arrow to actionBar
+        Toolbar toolBar = view.findViewById(R.id.fragment_create_task_toolbar);
+        toolBar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.clearFocus(); // Remove keyboard from view
+                // Go back to menu
+                Navigation.findNavController(view).popBackStack(R.id.createTask, true);
+            }
+        });
+
+        // Add button listener to add task.
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Get all resources
-
-                // Add back arrow to actionBar
-                Toolbar toolBar = view.findViewById(R.id.fragment_edit_task_toolbar);
-                toolBar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
-                toolBar.setNavigationOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        view.clearFocus(); // Remove keyboard from view
-                        // Go back to menu
-                        Navigation.findNavController(view).popBackStack(R.id.createTask, true);
-                    }
-                });
-
                 // Title
                 EditText nameField = view.findViewById(R.id.fragment_add_task_name_field);
                 String titleText = nameField.getText().toString();
@@ -56,6 +57,17 @@ public class CreateTask extends Fragment {
                 // Completed
                 CheckBox completedField = view.findViewById(R.id.fragment_add_task_completed_field);
                 boolean completed = completedField.isChecked();
+
+                // Format texts
+                titleText = titleText.trim();
+                descriptionText = descriptionText.trim();
+
+                //Check that title is not empty
+                if(titleText.length() == 0) {
+                    Toast toast = Toast.makeText(getContext(), R.string.fragment_create_task_toast, Toast.LENGTH_SHORT);
+                    toast.show();
+                    return; // Don't continue and input into database
+                }
 
                 //Input into database
                 TasksHelper tasksHelper = new TasksHelper(getContext());
