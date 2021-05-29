9db5a060a748d17dc0d5d9b051e8df1d53bfcf76
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-05-29 10:42:32 +0100

    Added hiding and showing of description of task

--- app/src/main/java/com/example/todo/TaskAdapter.java
+++ app/src/main/java/com/example/todo/TaskAdapter.java
@@ -10,6 +10,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import androidx.appcompat.widget.AppCompatImageButton;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -35,12 +38,19 @@ public class TaskAdapter extends
 
         public final TextView title;
         public final CheckBox done;
+        public final TextView desc;
+        public final TextView descTitle;
+        public final androidx.appcompat.widget.AppCompatImageButton expand;
+        public boolean expanded = false;
         final TaskAdapter taskAdapter;
 
         public TaskViewHolder(@NonNull View itemView, TaskAdapter taskAdapter) {
             super(itemView);
             title = itemView.findViewById(R.id.recycler_item_task_entry_title);
             done = itemView.findViewById(R.id.recycler_item_task_entry_done);
+            descTitle = itemView.findViewById(R.id.recycler_item_task_description);
+            desc = itemView.findViewById(R.id.recycler_item_task_description_contents);
+            expand = itemView.findViewById(R.id.recycler_item_task_entry_expand_button);
             this.taskAdapter = taskAdapter;
         }
     }
@@ -68,6 +78,8 @@ public class TaskAdapter extends
         // Set initial data
         holder.done.setChecked(currentTask.isDone());
         holder.title.setText(currentTask.getTaskName());
+        holder.desc.setText(currentTask.getTaskDesc());
+
 
         // Add listeners
         holder.done.setOnClickListener(new View.OnClickListener() {
@@ -92,12 +104,29 @@ public class TaskAdapter extends
                 Navigation.findNavController(view).navigate(R.id.action_toDoMainMenu_to_editTask);
             }
         });
+
+        holder.expand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("AAAAAAAAAAAAAAAAa", "expanded");
+                if(!holder.expanded) {
+                    holder.descTitle.setVisibility(View.VISIBLE);
+                    holder.desc.setVisibility(View.VISIBLE);
+                } else {
+                    holder.descTitle.setVisibility(View.GONE);
+                    holder.desc.setVisibility(View.GONE);
+                }
+                holder.expanded = !holder.expanded;
+                view.requestLayout();
+                //view.invalidate();
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
         return Tasks.size();
     }
-
 
 }
